6b8ccbf68a09e36b8231556fdf095d3155a824ad
James Abley <james.abley@gmail.com>
2018-11-27 09:22:37 +0000

    Add first failing test
    * Add sketch for token definitions
    * Add failing test for lexing

---
+++ go/src/monkey/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifier + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
