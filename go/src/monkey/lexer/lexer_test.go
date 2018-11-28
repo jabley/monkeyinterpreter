de4f117f1b4d1b9c37d5db2ff9ff3d0eb58f02a1
James Abley <james.abley@gmail.com>
2018-11-28 08:43:32 +0000

    Add lexing for other operators

--- go/src/monkey/lexer/lexer_test.go
+++ go/src/monkey/lexer/lexer_test.go
@@ -35,6 +35,9 @@ let add = fn( x, y) {
 };
 
 let result = add( five, ten);
+
+!-/* 5;
+5 < 10 > 5;
 `
 
 	testLexing(t, input, []expectedToken{
@@ -73,6 +76,18 @@ let result = add( five, ten);
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	})
