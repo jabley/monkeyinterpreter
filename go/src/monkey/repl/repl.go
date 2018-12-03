277dd7413912160dda17753f8b94fcc9e88a00a3
James Abley <james.abley@gmail.com>
2018-12-03 20:43:58 +0000

    Change the REPL to output the parsed AST

--- go/src/monkey/repl/repl.go
+++ go/src/monkey/repl/repl.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
-const Prompt = ">> "
+const prompt = ">> "
 
+// Start the loop of the REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Fprintf(out, Prompt)
+		fmt.Fprintf(out, prompt)
 		scanned := scanner.Scan()
 
 		if !scanned {
@@ -23,9 +24,39 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "% + v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
 
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const monkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, monkeyFace)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, fmt.Sprintf("\t%s\n", msg))
 	}
 }
