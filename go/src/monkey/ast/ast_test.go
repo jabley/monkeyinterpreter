037e3dc388786f6edd33842e1bf2f303ab8b0fdc
James Abley <james.abley@gmail.com>
2018-12-03 13:45:06 +0000

    Add test coverage for ast package
    Nice to have tests for code, rather than just assuming it works...

--- go/src/monkey/ast/ast_test.go
+++ go/src/monkey/ast/ast_test.go
@@ -44,3 +44,49 @@ func TestReturnString(t *testing.T) {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
+
+func TestInfixString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&ExpressionStatement{
+				Expression: &InfixExpression{
+					Token: token.Token{Type: token.Plus, Literal: "+"},
+					Left: &IntegerLiteral{
+						Token: token.Token{Type: token.Ident, Literal: "5"},
+						Value: 5,
+					},
+					Operator: "+",
+					Right: &IntegerLiteral{
+						Token: token.Token{Type: token.Ident, Literal: "10"},
+						Value: 10,
+					},
+				},
+			},
+		},
+	}
+
+	if program.String() != "(5 + 10)" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
+
+func TestPrefixString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&ExpressionStatement{
+				Expression: &PrefixExpression{
+					Token:    token.Token{Type: token.Plus, Literal: "+"},
+					Operator: "+",
+					Right: &IntegerLiteral{
+						Token: token.Token{Type: token.Ident, Literal: "10"},
+						Value: 10,
+					},
+				},
+			},
+		},
+	}
+
+	if program.String() != "(+10)" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
