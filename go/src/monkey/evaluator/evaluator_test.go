1b7710eebaa4c6ea3d2979940e804ade65122802
James Abley <james.abley@gmail.com>
2018-12-09 22:14:59 +0000

    Add failing test for evaluating bangs

--- go/src/monkey/evaluator/evaluator_test.go
+++ go/src/monkey/evaluator/evaluator_test.go
@@ -42,6 +42,27 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.input), func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			testBooleanObject(t, evaluated, tt.expected)
+		})
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
