99b7cf4a7c2c2e94072fca33a38c63defa5097a2
James Abley <james.abley@gmail.com>
2019-01-01 22:37:02 +0000

    Add array literal to the language

--- go/src/monkey/ast/ast.go
+++ go/src/monkey/ast/ast.go
@@ -48,6 +48,38 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+// ArrayLiteral allows for array support.
+type ArrayLiteral struct {
+	Token    token.Token
+	Elements []Expression
+}
+
+// String Node implementation
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
+
+// TokenLiteral returns the result of the root Node
+func (al *ArrayLiteral) TokenLiteral() string {
+	return al.Token.Literal
+}
+
+func (al *ArrayLiteral) expressionNode() {
+
+}
+
 // BlockStatement allows for blocks of code (as part of an `if` expression, for example)
 type BlockStatement struct {
 	Token      token.Token // the `{` token
