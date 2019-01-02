e528aeb8e650b177d2f6eae2f50f7223478839bd
James Abley <james.abley@gmail.com>
2019-01-02 16:53:49 +0000

    Add macro expansion
    This adds the final building block for having macros in our interpreter.

--- go/src/monkey/evaluator/macro_expansion_test.go
+++ go/src/monkey/evaluator/macro_expansion_test.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/object"
@@ -60,6 +61,46 @@ func TestDefineMacros(t *testing.T) {
 
 	if macro.Body.String() != expectedBody {
 		t.Fatalf("body is not %q. got=%q", expectedBody, macro.Body.String())
+	}
+}
+
+func TestExpandMacros(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`
+            let infixExpression = macro() { quote(1 + 2); };
+
+            infixExpression();
+            `,
+			`(1 + 2)`,
+		},
+		{
+			`
+            let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
+
+            reverse(2 + 2, 10 - 5);
+            `,
+			`(10 - 5) - (2 + 2)`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.input), func(t *testing.T) {
+			expected := testParseProgram(tt.expected)
+			program := testParseProgram(tt.input)
+
+			env := object.NewEnvironment()
+			DefineMacros(program, env)
+			expanded := ExpandMacros(program, env)
+
+			if expanded.String() != expected.String() {
+				t.Errorf("not equal. want=%q, got=%q",
+					expected.String(), expanded.String())
+			}
+		})
 	}
 }
 
