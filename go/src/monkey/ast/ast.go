2dcabc1bd265295f7346e8c6fc7cb159cc6886dc
James Abley <james.abley@gmail.com>
2019-01-02 16:40:37 +0000

    Add failing test for parsing macros

--- go/src/monkey/ast/ast.go
+++ go/src/monkey/ast/ast.go
@@ -387,6 +387,36 @@ func (ie *InfixExpression) TokenLiteral() string {
 }
 func (ie *InfixExpression) expressionNode() {}
 
+// MacroLiteral is the macro in Monkey
+type MacroLiteral struct {
+	Token      token.Token // The 'macro' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+// String Node implementation
+func (ml *MacroLiteral) String() string {
+	var out strings.Builder
+
+	params := []string{}
+	for _, p := range ml.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(ml.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(ml.Body.String())
+
+	return out.String()
+}
+
+// TokenLiteral Node implementation
+func (ml *MacroLiteral) TokenLiteral() string { return ml.Token.Literal }
+
+func (ml *MacroLiteral) expressionNode() {}
+
 // PrefixExpression is a Node for `!X` or `-X`
 type PrefixExpression struct {
 	Right    Expression
