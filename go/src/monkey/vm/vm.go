707058e7fa979de989638462b59506a76070d28f
James Abley <james.abley@gmail.com>
2019-04-03 09:24:46 +0100

    Add decode and execute to the VM
    This allows us to push `object.Object`s onto the stack.

--- go/src/monkey/vm/vm.go
+++ go/src/monkey/vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"monkey/code"
 	"monkey/compiler"
 	"monkey/object"
@@ -43,9 +44,27 @@ func (vm *VM) Run() error {
 		op := code.Opcode(vm.instructions[ip])
 
 		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
 
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
 		}
 	}
+
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("Stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
 
 	return nil
 }
