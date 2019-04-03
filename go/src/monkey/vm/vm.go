3c94fdfabaac25609ed8eda962fd6bd7596930ec
James Abley <james.abley@gmail.com>
2019-04-03 09:16:08 +0100

    Add failing test for VM
    The VM is what is going to do the fetch, decode, and execute cycle on
    the instructions.
    
    This adds the initial outline which does the fetch.
    
    ```sh
    --- FAIL: TestIntegerArithmetic (0.00s)
        --- FAIL: TestIntegerArithmetic/1 (0.00s)
            vm_test.go:54: object is not Integer. got=<nil> (<nil>)
        --- FAIL: TestIntegerArithmetic/2 (0.00s)
            vm_test.go:54: object is not Integer. got=<nil> (<nil>)
    ```

---
+++ go/src/monkey/vm/vm.go
@@ -0,0 +1,51 @@
+package vm
+
+import (
+	"monkey/code"
+	"monkey/compiler"
+	"monkey/object"
+)
+
+// StackSize is the hard limit for how deep we can go
+const StackSize = 2048
+
+// VM is responsible for executing bytecode. It will do the fetch, decode, and execute of instructions.
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // aka stack pointer. Always points to the next value. Top of stack is `stack[sp-1]`
+}
+
+// New returns a new VM ready to execute the specified bytecode.
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants:    bytecode.Constants,
+
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+	}
+}
+
+// StackTop returns the object.Object at the top of the stack, or nil if there isn't one.
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Run does the fetch, decode, and execute cycle on the instructions
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+
+		}
+	}
+
+	return nil
+}
---
+++ go/src/monkey/vm/vm_test.go
@@ -0,0 +1,78 @@
+package vm
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/compiler"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+	}
+
+	runVMTests(t, tests)
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func runVMTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf(`%v`, tt.input), func(t *testing.T) {
+			program := parse(tt.input)
+
+			comp := compiler.New()
+			err := comp.Compile(program)
+			if err != nil {
+				t.Fatalf("compiler error: %s", err)
+			}
+
+			vm := New(comp.Bytecode())
+			err = vm.Run()
+			if err != nil {
+				t.Fatalf("vm error: %s", err)
+			}
+
+			stackElem := vm.StackTop()
+			testExpectedObject(t, tt.expected, stackElem)
+		})
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		testIntegerObject(t, int64(expected), actual)
+	}
+}
+
+func testIntegerObject(t *testing.T, expected int64, actual object.Object) {
+	t.Helper()
+
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		t.Fatalf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		t.Fatalf("object has the wrong value. got=%d, want=%d", result.Value, expected)
+	}
+}
