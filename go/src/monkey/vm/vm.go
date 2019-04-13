581413c52821a496df10bd602012861e2f16dbc5
James Abley <james.abley@gmail.com>
2019-04-13 11:07:19 +0200

    Add support for conditionals to the VM

--- go/src/monkey/vm/vm.go
+++ go/src/monkey/vm/vm.go
@@ -94,6 +94,17 @@ func (vm *VM) Run() error {
 			}
 		case code.OpPop:
 			vm.pop()
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
 		}
 	}
 
@@ -209,6 +220,15 @@ func (vm *VM) push(o object.Object) error {
 	vm.sp++
 
 	return nil
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
 
 func nativeBoolToBooleanObject(b bool) *object.Boolean {
