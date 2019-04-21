99bd781f301b9031c1be9559eef21a02e7b318fb
James Abley <james.abley@gmail.com>
2019-04-21 22:14:22 +0100

    Add VM test showing more first-class functions
    Functions can return named functions.

--- go/src/monkey/vm/vm_test.go
+++ go/src/monkey/vm/vm_test.go
@@ -245,6 +245,16 @@ func TestFirstClassFunctions(t *testing.T) {
 		`,
 			expected: 1,
 		},
+		{
+			input: `
+		let returnsOneReturner = fn() {
+			let returnsOne = fn() { 1; };
+			returnsOne;
+		};
+		returnsOneReturner()();
+		`,
+			expected: 1,
+		},
 	}
 
 	runVMTests(t, tests)
