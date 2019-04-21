a179b80565817b4a2ae5cf69a735f690976d8b72
James Abley <james.abley@gmail.com>
2019-04-21 14:13:11 +0100

    Add more tests to the VM function calls
    Multiple functions, sequential or nested, explicit early returns...

--- go/src/monkey/vm/vm_test.go
+++ go/src/monkey/vm/vm_test.go
@@ -170,6 +170,43 @@ func TestCallingFunctionsWithoutArguments(t *testing.T) {
 		`,
 			expected: 15,
 		},
+		{
+			input: `
+			let one = fn() { 1; };
+			let two = fn() { 2; };
+			one() + two()
+			`,
+			expected: 3,
+		},
+		{
+			input: `
+					let a = fn() { 1 };
+					let b = fn() { a() + 1 };
+					let c = fn() { b() + 1 };
+					c();
+					`,
+			expected: 3,
+		},
+	}
+
+	runVMTests(t, tests)
+}
+
+func TestFunctionsWithReturnStatements(t *testing.T) {
+	tests := []vmTestCase{{
+		input: `
+		let earlyExit = fn() { return 99; 100; };
+		earlyExit();
+		`,
+		expected: 99,
+	},
+		{
+			input: `
+			 let earlyExit = fn() { return 99; return 100; };
+			 earlyExit();
+			 `,
+			expected: 99,
+		},
 	}
 
 	runVMTests(t, tests)
