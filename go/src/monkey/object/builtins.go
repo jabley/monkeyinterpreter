455588ee9062327fb72ea9b1d83f2768bc1d093c
James Abley <james.abley@gmail.com>
2019-04-22 09:17:23 +0100

    Refactor `first`, `last`, and `rest` builtins

--- go/src/monkey/object/builtins.go
+++ go/src/monkey/object/builtins.go
@@ -38,6 +38,76 @@ var BuiltIns = []struct {
 			},
 		},
 	},
+	{
+		"first",
+		&BuiltIn{
+			Fn: func(args ...Object) Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1",
+						len(args))
+				}
+				if args[0].Type() != ArrayObj {
+					return newError("argument to `first` must be ARRAY, got %s",
+						args[0].Type())
+				}
+
+				arr := args[0].(*Array)
+				if len(arr.Elements) > 0 {
+					return arr.Elements[0]
+				}
+
+				return nil
+			},
+		},
+	},
+	{
+		"last",
+		&BuiltIn{
+			Fn: func(args ...Object) Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1",
+						len(args))
+				}
+				if args[0].Type() != ArrayObj {
+					return newError("argument to `last` must be ARRAY, got %s",
+						args[0].Type())
+				}
+
+				arr := args[0].(*Array)
+				length := len(arr.Elements)
+				if length > 0 {
+					return arr.Elements[length-1]
+				}
+
+				return nil
+			},
+		},
+	},
+	{
+		"rest",
+		&BuiltIn{
+			Fn: func(args ...Object) Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1",
+						len(args))
+				}
+				if args[0].Type() != ArrayObj {
+					return newError("argument to `rest` must be ARRAY, got %s",
+						args[0].Type())
+				}
+
+				arr := args[0].(*Array)
+				length := len(arr.Elements)
+				if length > 0 {
+					newElements := make([]Object, length-1, length-1)
+					copy(newElements, arr.Elements[1:length])
+					return &Array{Elements: newElements}
+				}
+
+				return nil
+			},
+		},
+	},
 }
 
 // GetBuiltInByName returns the named BuiltIn, or nil if there is no such BuiltIn
