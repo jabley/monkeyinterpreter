2e2289b0d77509c90a7fa5c53ab83bd9e5348146
James Abley <james.abley@gmail.com>
2019-04-22 09:10:03 +0100

    Refactor builtins
    Move to the object package, so that they can be used by both the
    evaluator and the VM.

---
+++ go/src/monkey/object/builtins.go
@@ -0,0 +1,43 @@
+package object
+
+import "fmt"
+
+// BuiltIns is the set of builtin functions usable by both the VM and evaluator
+var BuiltIns = []struct {
+	Name    string
+	BuiltIn *BuiltIn
+}{
+	{
+		"len",
+		&BuiltIn{
+			Fn: func(args ...Object) Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1", len(args))
+				}
+
+				switch arg := args[0].(type) {
+				case *Array:
+					return &Integer{Value: int64(len(arg.Elements))}
+				case *String:
+					return &Integer{Value: int64(len(arg.Value))}
+				default:
+					return newError("argument to `len` not supported, got %s", args[0].Type())
+				}
+			},
+		},
+	},
+}
+
+// GetBuiltInByName returns the named BuiltIn, or nil if there is no such BuiltIn
+func GetBuiltInByName(name string) *BuiltIn {
+	for _, def := range BuiltIns {
+		if def.Name == name {
+			return def.BuiltIn
+		}
+	}
+	return nil
+}
+
+func newError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
