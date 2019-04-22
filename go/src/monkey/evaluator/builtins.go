705d9d5ef58ef82946e79fb8f7c6c9b681322d7f
James Abley <james.abley@gmail.com>
2019-04-22 09:20:56 +0100

    Refactor `push` builtin

--- go/src/monkey/evaluator/builtins.go
+++ go/src/monkey/evaluator/builtins.go
@@ -10,25 +10,5 @@ var builtins = map[string]*object.BuiltIn{
 	"first": object.GetBuiltInByName("first"),
 	"last":  object.GetBuiltInByName("last"),
 	"rest":  object.GetBuiltInByName("rest"),
-	"push": &object.BuiltIn{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2",
-					len(args))
-			}
-			if args[0].Type() != object.ArrayObj {
-				return newError("argument to `push` must be ARRAY, got %s",
-					args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-
-			newElements := make([]object.Object, length+1, length+1)
-			copy(newElements, arr.Elements)
-			newElements[length] = args[1]
-
-			return &object.Array{Elements: newElements}
-		},
-	},
+	"push":  object.GetBuiltInByName("push"),
 }
--- go/src/monkey/object/builtins.go
+++ go/src/monkey/object/builtins.go
@@ -108,6 +108,30 @@ var BuiltIns = []struct {
 			},
 		},
 	},
+	{
+		"push",
+		&BuiltIn{
+			Fn: func(args ...Object) Object {
+				if len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2",
+						len(args))
+				}
+				if args[0].Type() != ArrayObj {
+					return newError("argument to `push` must be ARRAY, got %s",
+						args[0].Type())
+				}
+
+				arr := args[0].(*Array)
+				length := len(arr.Elements)
+
+				newElements := make([]Object, length+1, length+1)
+				copy(newElements, arr.Elements)
+				newElements[length] = args[1]
+
+				return &Array{Elements: newElements}
+			},
+		},
+	},
 }
 
 // GetBuiltInByName returns the named BuiltIn, or nil if there is no such BuiltIn
