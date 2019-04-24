a6a801885d656d9570aaf5f3a8fbb8d883c9799e
James Abley <james.abley@gmail.com>
2019-04-24 08:53:33 +0100

    Fix compiling closures
    * Emit `OpGetFree` instructions to load free variables correctly
    * Emit the correct number of free variables as part of `OpClosure`

--- go/src/monkey/compiler/compiler.go
+++ go/src/monkey/compiler/compiler.go
@@ -275,8 +275,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpReturn)
 		}
 
+		freeSymbols := c.symbolTable.FreeSymbols
 		numLocals := c.symbolTable.numDefinitions
 		instructions := c.leaveScope()
+
+		for _, s := range freeSymbols {
+			c.loadSymbol(s)
+		}
 
 		compiledFn := &object.CompiledFunction{
 			Instructions:  instructions,
@@ -284,7 +289,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 			NumParameters: len(node.Parameters),
 		}
 		fnIndex := c.addConstant(compiledFn)
-		c.emit(code.OpClosure, fnIndex, 0)
+		c.emit(code.OpClosure, fnIndex, len(freeSymbols))
 
 	case *ast.ReturnStatement:
 		if err := c.Compile(node.ReturnValue); err != nil {
@@ -392,6 +397,8 @@ func (c *Compiler) loadSymbol(s Symbol) {
 		c.emit(code.OpGetLocal, s.Index)
 	case BuiltInScope:
 		c.emit(code.OpGetBuiltIn, s.Index)
+	case FreeScope:
+		c.emit(code.OpGetFree, s.Index)
 	}
 }
 
