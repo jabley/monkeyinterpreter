56e63c3da8389249d2df80f749ecb44d8ee7411e
James Abley <james.abley@gmail.com>
2020-08-03 23:26:36 +0100

    Add REPL in Rust
    For now, it just lexes.

--- rust/rmonkey/src/main.rs
+++ rust/rmonkey/src/main.rs
@@ -1,3 +1,33 @@
+pub mod lexer;
+pub mod token;
+
+use std::io;
+use std::io::BufRead;
+use std::io::Write;
+
+use lexer::Lexer;
+use token::Token;
+
 fn main() {
-    println!("Hello, monkey!");
+    let stdin = io::stdin();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin
+            .lock()
+            .read_line(&mut line)
+            .expect("Error reading from stdin");
+        let mut lexer = Lexer::new(&line);
+
+        loop {
+            let tok = lexer.next_token();
+            println!("{:?}", tok);
+            if tok == Token::Eof {
+                break;
+            }
+        }
+    }
 }
