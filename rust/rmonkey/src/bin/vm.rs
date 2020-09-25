fabd13ca047163e50423ef59eff3ee1876b12462
James Abley <james.abley@gmail.com>
2020-09-25 10:23:11 +0100

    Try to use pprof-rs to generate flamegraphs
        > cargo run --bin vm-flamegraph --release -- -p profile.pb
            Finished release [optimized + debuginfo] target(s) in 0.38s
             Running `target/release/vm-flamegraph -p profile.pb`
        Result: 9227465
        report: THREAD: ThreadId(4437034432) 2763
    
    And
    
        > go tool pprof -http=":8080" profile.pb
    
    Doesn't seem to be working though. I'm not sure what I'm doing wrong.
    You can see there is only a single THREAD being dumped.
    
    From the example in https://github.com/tikv/pprof-rs/blob/24c6df49e7f15f45827fed55a56871342caf7739/examples/profile_proto.rs
    it doesn't look like I need to do more. But the output only contains one
    item - there is nothing that usefully gets rendered in the flamegraph.

--- rust/rmonkey/src/bin/vm.rs
+++ rust/rmonkey/src/bin/vm.rs
@@ -1,6 +1,26 @@
+use std::{fs::File, io::Write};
+
+use clap::{App, Arg};
+use pprof;
+use pprof::protos::Message;
 use rmonkey::{ast::Program, compiler::Compiler, lexer::Lexer, parser::Parser, vm::VM};
 
 fn main() {
+    let matches = App::new("VM example")
+        .arg(
+            Arg::with_name("cpuprofile")
+                .short("p")
+                .long("cpuprofile")
+                .help("Optional flag indicating whether to save pprof CPU profile")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let guard = match matches.value_of("cpuprofile") {
+        Some(_) => pprof::ProfilerGuard::new(97).ok(),
+        None => None,
+    };
+
     let program = parse();
 
     let mut c = Compiler::new();
@@ -16,6 +36,21 @@ fn main() {
         }
         Err(e) => println!("Unexpected error: {}", e.to_string()),
     }
+
+    guard.and_then(|guard| -> Option<()> {
+        if let Ok(report) = guard.report().build() {
+            let mut file = File::create(matches.value_of("cpuprofile").unwrap()).unwrap();
+            let profile = report.pprof().unwrap();
+
+            let mut content = Vec::new();
+            profile.encode(&mut content).unwrap();
+            file.write_all(&content).unwrap();
+
+            println!("report: {}", &report);
+        };
+
+        None
+    });
 }
 
 fn parse() -> Program {
